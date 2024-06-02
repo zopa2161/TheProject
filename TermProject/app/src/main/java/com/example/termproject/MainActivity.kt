12f46635b48f26f3e80656ff2fafc747d934bbff
zopa2161 <126738169+zopa2161@users.noreply.github.com>
2024-06-02 15:05:09 +0900

    1

--- TermProject/app/src/main/java/com/example/termproject/MainActivity.kt
+++ TermProject/app/src/main/java/com/example/termproject/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.termproject.ui.theme.TermProjectTheme
+//수정하기
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
