c6ec8fe1598ba6741c31ff1756ed8644913395ef
zopa2161 <126738169+zopa2161@users.noreply.github.com>
2024-06-02 17:52:58 +0900

    Update MainActivity.kt

--- TermProject/app/src/main/java/com/example/termproject/MainActivity.kt
+++ TermProject/app/src/main/java/com/example/termproject/MainActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.termproject.ui.theme.TermProjectTheme
-//수정하기 수정해보기
+//수정하기 수정해보기 ㅅㅅㅅㅅㅅㅅㅅ
 
 class MainActivity : AppCompatActivity() {
     var database :SQLiteDatabase? = null
